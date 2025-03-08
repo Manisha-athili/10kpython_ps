0bcad75f5a40cb0f4d8cb0721df94611fd0d3baa
Manisha-athili <manishaathili1126@gmail.com>
2025-03-08 15:30:31 +0530

    add

---
+++ day09-Revision/while.py
@@ -0,0 +1,38 @@
+# i = 1
+# while i<=50:
+#     if i%2==0:
+#         print(i)
+#     i += 1    
+   
+# i = 2
+# while i<=50:
+#     print(i)
+#     i += 2   
+
+# multiplication
+
+# i = 1
+# while i<=20:
+#     j = 1
+#     while j<=10:
+#         print(f"{i} * {j} = {i*j}")
+#         j += 1
+#     print("********",i)
+#     i += 1
+
+# print("reverse a number and print sum")
+
+# num = 2546
+# sum = 0
+# while num>0:
+
+
+num = int(input("enter n number : "))
+for num1 in range(0,5):
+    i = 1
+    while i<=num:
+        print("* ",end="")
+        i +=1
+    print()
+    
+    
